3d3c13bac2017ba0897f2d8f4f82ba6b0274451a
Kaspar de Geus <kaspar.de.geus@npo.nl>
2024-03-13 18:01:44 +0100

    [PLAYER-1068] - Fixing broken flavors

--- SampleApp/src/DEV/java/nl/npo/player/sample_app/data/ads/AdManagerProvider.kt
+++ SampleApp/src/DEV/java/nl/npo/player/sample_app/data/ads/AdManagerProvider.kt
@@ -1,8 +1,9 @@
 package nl.npo.player.sample_app.data.ads
 
+import android.content.Context
 import nl.npo.player.library.domain.ads.AdManager
 import nl.npo.player.library.domain.ads.NoAdManager
 
 object AdManagerProvider {
-    fun getAdManager(): AdManager = NoAdManager()
+    fun getAdManager(_context: Context): AdManager = NoAdManager()
 }
