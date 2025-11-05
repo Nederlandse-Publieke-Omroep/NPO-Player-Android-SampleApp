3fde7386bddfbd0124bed6211cd0c367a66ff747
roma-npo <roma.morel@npo.nl>
2025-11-05 15:09:24 +0100

    spotless check

--- SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/data/link/URLLinkDataRepository.kt
+++ SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/data/link/URLLinkDataRepository.kt
@@ -140,7 +140,6 @@ object URLLinkDataRepository : LinkRepository {
                 avType = AVType.VIDEO,
                 isLive = true,
             ),
-
             SourceWrapper(
                 title = "Test- Audio stream",
                 streamUrl = "https://dev-tommie.cdn.streamgate.nl/video/live/audio/audio.isml/.mpd",
@@ -148,7 +147,7 @@ object URLLinkDataRepository : LinkRepository {
                 getStreamLink = false,
                 imageUrl =
                     "https://content.radioveronica.nl/images/66qysnomiif1/3Cy1DKrJZpB4uq4CHxA5d1/" +
-                            "31a2820a631bdea44fe6bdcfd054ec68/Radio-Veronica_Martijn-Muijs_1200x675.jpg?fit=thumb&w=774&h=465&fm=webp",
+                        "31a2820a631bdea44fe6bdcfd054ec68/Radio-Veronica_Martijn-Muijs_1200x675.jpg?fit=thumb&w=774&h=465&fm=webp",
                 avType = AVType.AUDIO,
                 isLive = false,
             ),
