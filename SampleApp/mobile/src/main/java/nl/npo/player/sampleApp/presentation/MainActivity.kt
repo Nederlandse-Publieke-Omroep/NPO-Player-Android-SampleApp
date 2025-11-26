0272396c24bee71a022a62ebd31456520925312c
roma-npo <roma.morel@npo.nl>
2025-11-26 08:42:28 +0100

    removed unused code

--- SampleApp/mobile/src/main/java/nl/npo/player/sampleApp/presentation/MainActivity.kt
+++ SampleApp/mobile/src/main/java/nl/npo/player/sampleApp/presentation/MainActivity.kt
@@ -73,32 +73,6 @@ class MainActivity : BaseActivity() {
         }
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val menuInflater = menuInflater
-// //        menuInflater.inflate(R.menu.menu_activity_main, menu)
-//
-//        menu.findItem(R.id.media_route_menu_item).isVisible = NPOCasting.isCastingEnabled
-//
-//        // Adding a Cast Button in the menu bar
-//        if (NPOCasting.isCastingEnabled) {
-//            CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item)
-//        }
-//        return true
-//    }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        if (item.itemId == R.id.menu_settings) {
-//            showSettings()
-//            return true
-//        }
-//
-//        return super.onOptionsItemSelected(item)
-//    }
-//
-//    private fun showSettings() {
-//        SettingsBottomSheetDialog.newInstance().show(supportFragmentManager, null)
-//    }
-
     private val requestPermissionLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestPermission(),
