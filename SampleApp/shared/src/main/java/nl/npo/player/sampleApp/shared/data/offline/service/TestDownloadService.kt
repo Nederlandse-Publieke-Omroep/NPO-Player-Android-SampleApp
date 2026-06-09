8a1771ceae5ada3354bb87c0e17c1c29dd2a2338
Kaspar de Geus <kaspar.de.geus@npo.nl>
2026-06-09 12:49:40 +0200

    [PLAYER-3426] - Updated to latest library snapshot version

--- SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/data/offline/service/TestDownloadService.kt
+++ SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/data/offline/service/TestDownloadService.kt
@@ -1,17 +1,5 @@
 package nl.npo.player.sampleApp.shared.data.offline.service
 
-import android.R.attr.progress
-import android.app.Notification
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import androidx.annotation.OptIn
-import androidx.core.app.NotificationCompat
-import androidx.media3.common.C
-import androidx.media3.common.util.Log
-import androidx.media3.common.util.UnstableApi
-import androidx.media3.exoplayer.offline.Download
-import nl.npo.player.library.data.offline.exoplayer.NPODownloadService
-import nl.npo.player.library.domain.offline.models.NPODownloadState
-import nl.npo.player.sampleApp.shared.R
+import nl.npo.player.library.presentation.offline.NPODownloadService
 
 class TestDownloadService : NPODownloadService()
