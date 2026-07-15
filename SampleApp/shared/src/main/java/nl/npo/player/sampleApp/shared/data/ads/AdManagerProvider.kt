0abcb80f3d18a0152453ee5844e78e4ffa495e40
Kaspar de Geus <kaspar.de.geus@npo.nl>
2026-07-15 16:08:19 +0200

    [PLAYER-3722] - Replaced Ima with Vast ad manager

--- SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/data/ads/AdManagerProvider.kt
+++ SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/data/ads/AdManagerProvider.kt
@@ -3,8 +3,8 @@ package nl.npo.player.sampleApp.shared.data.ads
 import android.content.Context
 import nl.npo.player.library.domain.ads.AdManager
 import nl.npo.player.library.domain.ads.SterConfiguration
-import nl.npo.player.library.sterads.presentation.ads.ImaAdManager
+import nl.npo.player.library.sterads.presentation.ads.VastAdManager
 
 object AdManagerProvider {
-    fun getAdManager(applicationContext: Context): AdManager = ImaAdManager(SterConfiguration(applicationContext.packageName))
+    fun getAdManager(applicationContext: Context): AdManager = VastAdManager(SterConfiguration(applicationContext.packageName))
 }
