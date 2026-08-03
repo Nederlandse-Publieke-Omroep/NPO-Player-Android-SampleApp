fe4854fe493eb51022267ef950f751671ce09e41
Kaspar de Geus <kaspar.de.geus@npo.nl>
2026-08-03 13:08:49 +0200

    - Fix: Added back the interceptors (Chucker) so we can see the network traffic

--- SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/app/SampleApplication.kt
+++ SampleApp/shared/src/main/java/nl/npo/player/sampleApp/shared/app/SampleApplication.kt
@@ -50,6 +50,7 @@ open class SampleApplication :
                         this.environment = environment
                         this.enableCasting = enableCasting
                         debugLogging = true
+                        addInterceptors(list)
                     }
                 }
         } else {
